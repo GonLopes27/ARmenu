50f9ac038dec1515040536c85aa607257e3fbcc5
a54916 <a54916@alunos.ipb.pt>
2025-06-12 21:54:59 +0100

    teste2

--- script/script.js
+++ script/script.js
@@ -284,8 +284,8 @@ document.getElementById('ar-btn').addEventListener('click', () => {
 
   const newViewer = document.createElement('model-viewer');
   newViewer.setAttribute('id', 'ar-viewer');
-  newViewer.setAttribute('src', 'assets/models/Duck.glb');
-  newViewer.setAttribute('ios-src', 'assets/models/Duck.usdz');
+  newViewer.setAttribute('src', 'assets/models/Estrogonofe.glb');
+  newViewer.setAttribute('ios-src', 'assets/models/Estrogonofe.usdz');
   newViewer.setAttribute('ar', '');
   newViewer.setAttribute('ar-modes', 'scene-viewer quick-look webxr');
   newViewer.setAttribute('camera-controls', '');
